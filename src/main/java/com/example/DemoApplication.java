e45cef39a0ebf130bf231899f8e94f9f9e67d23d
carrot <jaeshin.yu@desirelab.co.kr>
2016-04-25 17:45:53 +0900

    new line test

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -32,6 +32,7 @@ public class DemoApplication {
 			repository.save(new Customer("Jack", "Bauer"));
 			repository.save(new Customer("Chloe", "O'Brian"));
 			repository.save(new Customer("Kim", "Bauer"));
+
 			repository.save(new Customer("David", "Palmer"));
 			repository.save(new Customer("Michelle", "Dessler"));
 
