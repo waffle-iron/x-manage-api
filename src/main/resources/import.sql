670c1cc63ecd88192e0aaf0da216615b8e5e8870
carrot <jaeshin.yu@desirelab.co.kr>
2016-04-26 10:28:21 +0900

    init pj

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into customer (first_name, last_name) values ('first', 'last')
-insert into customer (first_name, last_name) values ('second', 'last')
-insert into customer (first_name, last_name) values ('third', 'last')
